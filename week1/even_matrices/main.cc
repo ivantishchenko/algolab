fb5525c8567ece8accdc619d09563e5df880b1d9
Ivan Tishchenko <johnny.tishchenko@gmail.com>
2020-09-28 17:45:49 +0200

    Implemented even matrices in O(n^3)

--- week1/even_matrices/main.cc
+++ week1/even_matrices/main.cc
@@ -16,36 +16,37 @@ void print_matr(vector<vector<int>> &matr, int n) {
 void testcase() {
     int n;
     cin >> n;
-    vector<vector<int>> matr;
+    vector<vector<int>> matr(n, vector<int>(n));
     for(int i = 0; i < n; i++) {
-        vector<int> row;
         for(int j = 0; j < n; j++) {
-            int x;
-            cin >> x;
-            row.push_back(x);
+            cin >> matr[i][j];    
         }
-        matr.push_back(row);
     }
-
-    vector<vector<int>> sums(n + 1, vector<int>(n + 1, 0));
-
+    vector<vector<int>> sums(n + 1, vector<int>(n + 1));
     for(int i = 1; i <= n; i++) {
         for(int j = 1; j <= n; j++) {
             sums[i][j] = sums[i - 1][j] + sums[i][j - 1] - sums[i - 1][j - 1] + matr[i - 1][j - 1];
-        }        
+        }
     }
     int res = 0;
-    // print_matr(row_sums, n);
     for(int i1 = 1; i1 <= n; i1++) {
-        for(int j1 = 1; j1 <= n; j1++) {
-            for(int i2 = i1; i2 <= n; i2++) {
-                for(int j2 = j1; j2 <= n; j2++) {
-                    int sum = sums[i2][j2] - sums[i1 - 1][j2] - sums[i2][j1 - 1] + sums[i1 - 1][j1 - 1];
-                    if (sum % 2 == 0) res++;
-                } 
-            }          
-        } 
+        for(int i2 = i1; i2 <= n; i2++) {
+            vector<int> p_sums(n + 1);
+            for(int k = 1; k <= n; k++) {
+               int sum = sums[i2][k] - sums[i1 - 1][k] - sums[i2][k - 1] + sums[i1 - 1][k - 1];
+               p_sums[k] = p_sums[k - 1] + sum;
+            }
+            int even = 0, odd = 0;
+            for(int j = 1; j <= n; j++) {
+                if(p_sums[j] % 2 == 0) even++;
+                else odd++;
+            }
+            res += even;
+            res += even * (even - 1) / 2;
+            res += odd * (odd - 1) / 2; 
+        }
     }
+
     cout << res << endl;
 }
 
