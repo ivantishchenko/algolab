8ea8403e306b02cec3494f301353d7baae62b1e0
Ivan Tishchenko <johnny.tishchenko@gmail.com>
2020-09-28 15:37:51 +0200

    Updated even matrices with pre computer

--- week1/even_matrices/main.cc
+++ week1/even_matrices/main.cc
@@ -3,6 +3,16 @@
 
 using namespace std;
 
+void print_matr(vector<vector<int>> &matr, int n) {
+    cout << endl;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << matr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
 void testcase() {
     int n;
     cin >> n;
@@ -16,20 +26,42 @@ void testcase() {
         }
         matr.push_back(row);
     }
+
+    vector<vector<int>> row_sums;
+    for(int i = 0; i < n; i++) {
+        vector<int> row;
+        row.push_back(matr[i][0]);
+        for(int j = 1; j < n; j++) {
+           row.push_back(matr[i][j] + row[j - 1]); 
+        }
+        row_sums.push_back(row);
+    }    
+    // print_matr(row_sums, n);
+    for(int j = 0; j < n; j++) {
+        for(int i = 1; i < n; i++) {
+            row_sums[i][j] += row_sums[i - 1][j];            
+        }
+    }
     int res = 0;
+    // print_matr(row_sums, n);
     for(int i1 = 0; i1 < n; i1++) {
         for(int j1 = 0; j1 < n; j1++) {
             for(int i2 = i1; i2 < n; i2++) {
                 for(int j2 = j1; j2 < n; j2++) {
-                    // calculate the sum
                     int sum = 0;
-                    for(int k = i1; k <= i2; k++) {
-                        for(int l = j1; l <= j2; l++) {
-                            sum += matr[k][l];
-                        }
-                    } 
+                    sum += row_sums[i2][j2];
+                    if(i1 != 0) sum -= row_sums[i1 - 1][j2];
+                    if(j1 != 0) sum -= row_sums[i2][j1 - 1];
+                    if(i1 != 0 && j1 != 0) sum += row_sums[i1 - 1][j1 - 1];
+                    // calculate the sum
+                    // int sum = 0;
+                    // for(int k = i1; k <= i2; k++) {
+                    //    for(int l = j1; l <= j2; l++) {
+                    //        sum += matr[k][l];
+                    //    }
+                    //} 
                     if (sum % 2 == 0) {
-                        res++;
+                       res++;
                     }
                 } 
             }          
